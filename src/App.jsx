f93675f8e9b6f3911531bdcfcbc319ace481197c
tablaheroes <fredimania@gmail.com>
2024-11-09 10:41:32 +0100

    commit f

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
 import React, { useState } from "react";
-import { Listarheroes } from "./components/Listarheroes"; // Importar el componente correctamente
-import "./Table.css"; // Importar los estilos
+import { Listarheroes } from "./components/Listarheroes"; 
+import "./Table.css"; 
 
 function App() {
-  const [publisher, setPublisher] = useState("DC Comics"); // Por defecto, 'DC Comics'
+  const [publisher, setPublisher] = useState("DC Comics"); // Por defecto, DC Comics o el que gustes 
 
   return (
     <div className="App">
-      <h1>Listado de Héroes</h1>
+      <h1>Listado Héroes de Fredi Cc</h1>
       <div>
         {/* Puedes cambiar el publisher usando botones o cualquier lógica */}
         <button onClick={() => setPublisher("DC Comics")}>DC Comics</button>
         <button onClick={() => setPublisher("Marvel Comics")}>Marvel Comics</button>
       </div>
 
-      {/* Aquí corregimos el nombre del componente */}
+      {/* corregimos nombre*/}
       <Listarheroes publisher={publisher} />
     </div>
   );
